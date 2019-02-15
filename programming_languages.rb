313d8bfb22c598943b2412c5cde8e366198ca6db
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 16:03:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,10 +3,10 @@ new_hash = {}
 languages.each do |name, language|
   language.each do |key, value|
     if new_hash.has_key?(key)
-      new_hash[key][:style] << style
+      new_hash[key][:style].push(name)
     else
       new_hash[key] = value
-      new_hash[key][:style] = [style]
+      new_hash[key][:style] = [name]
     end
   end
 end
