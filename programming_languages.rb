e9867887475e76b0855609b88833250939b1f32c
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 15:57:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |name, language|
     language.each do |type, value|
-    if new_hash.include?(name)
+    if new_hash.include?(type)
         new_hash[name][:style] << style
       else
         new_hash[name] = value
