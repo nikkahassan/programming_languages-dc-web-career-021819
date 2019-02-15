0a8cc9d71ba6cc5c15285b9a73ca85ad867ee4ee
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 16:02:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,14 @@
-
 def reformat_languages(languages)
-  new_hash = {}
-  languages.each do |name, language|
-    language.each do |type, value|
-    if new_hash.has_key?(type)
-        new_hash[type][:style] << style
-      else
-        new_hash[type] = value
-        new_hash[type][:style] = [style]
-      end
+new_hash = {}
+languages.each do |name, language|
+  language.each do |key, value|
+    if new_hash.include?(key)
+      new_hash[key][:style].push(style)
+    else
+      new_hash[key] = value
+      new_hash[key][:style] = [style]
     end
   end
-  return new_hash
+end
+new_hash
 end
